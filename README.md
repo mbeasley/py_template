db9e0c7d08142a5d7ae77fbd8b9eefe8e11f7ed4
Mike Beasley <mike.beasley@gmail.com>
2014-08-05 15:42:44 -0400

    Addded generic utility template

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ py_template
 ===========
 
 A template for Python CLI utlities
+
+This is almost an exact copy of the proposed CLI template presented
+in [Columbia's applied data science manual](http://columbia-applied-data-science.github.io/appdatasci.pdf),
+in chapter 3.2. I use it as a starter template for general purpose file processing scripts.
---
+++ util.py
@@ -0,0 +1,51 @@
+from optparse import OptionParser
+import sys
+
+def main():
+    r"""
+    DESCRIPTION
+    -----------
+    Put the description of what the utility does here.
+
+    EXAMPLES
+    --------
+    Describe example usage here.
+    python util.py [options] [file]
+    """
+    usage = "usage: %prog [options] dataset"
+    usage += '\n'+main.__doc__
+    parser = OptionParser(usage=usage)
+    parser.add_option(
+        "-n", "--deletion_rate",
+        help="Delete every nth line [default: %default] ",
+        action="store", dest='deletion_rate', type=float, default=2)
+
+    (options, args) = parser.parse_args()
+
+    ### Parse args
+    # Raise an exception if the length of args is greater than 1
+    assert len(args) <= 1
+    infilename = args[0] if args else None
+
+    ## Get the infile
+    if infilename:
+        infile = open(infilename, 'r')
+    else:
+        infile = sys.stdin
+
+    ## Call the function that does the real work
+    run(infile, sys.stdout, options)
+
+    ## Close the infile iff not stdin
+    if infilename:
+        infile.close()
+
+def run(infile, outfile, options):
+    """
+    Describe what's actually being done
+    """
+    for linenumber, line in enumerate(infile):
+        outfile.write(line)
+
+if __name__=='__main__':
+    main()
